395a84a1cd5b2bb5814cf887a134f27e8b05cde3
kdando <kvdando@gmail.com>
2024-08-19 23:59:26 +0100

    basic collision stops movement

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+
+    public PlayerMovement movement;
+
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacle")
         {
-            Debug.Log("HELLO");
+            movement.enabled = false;
         }
     }
 }
