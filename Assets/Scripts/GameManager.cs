d290049faa8abc3fbc4c8fb492f5f4baf55f2e13
kdando <kvdando@gmail.com>
2024-08-24 14:28:30 +0100

    added end of level trigger, animation and transition to next level

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,13 @@ public class GameManager : MonoBehaviour
 
     bool gameHasEnded = false;
     public float restartDelay = 2f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+
+    }
 
     public void EndGame()
     {
